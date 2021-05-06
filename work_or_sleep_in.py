b2334218e6738c53baa3ffc184aeaa47021c1f64
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-05-05 21:21:30 -0400

    added work_or_sleep_in.py

---
+++ work_or_sleep_in.py
@@ -0,0 +1,10 @@
+user_input = int(input("Choose a number between 0-6 to see if you should sleep in: "))
+
+if ((user_input > 0) and (user_input < 6)):
+    print("Its a weekday, you should go to work.")
+elif user_input == 0:
+    print("Its Sunday, you should sleep in.")
+elif user_input == 6:
+    print("Its Saturday, you should sleep in.")
+else:
+    print("Sorry, that number is greater than 6")
