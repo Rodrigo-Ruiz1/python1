4169b0a1f89cb6853d7a78d67d3f00356a7c3b94
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-05-05 21:21:10 -0400

    added day_of_week.py

---
+++ day_of_week.py
@@ -0,0 +1,20 @@
+#int will turn the user_input string into an integer, allowing us to compare it in our if/else statements
+user_input = int(input("Choose a number between 0-6 to see what day of the week it corresponds with: "))
+
+if user_input == 0:
+    print("%d corresponds with Sunday" % (user_input))
+elif user_input == 1:
+    print("%d corresponds with Monday" % (user_input))
+elif user_input == 2:
+    print("%d corresponds with Tuesday" % (user_input))
+elif user_input == 3:
+    print("%d corresponds with Wednesday" % (user_input))
+elif user_input == 4:
+    print("%d corresponds with Thursday" % (user_input))
+elif user_input == 5:
+    print("%d corresponds with Friday" % (user_input))
+elif user_input > 6:
+    print("Error: that number is greater than 6")
+else:
+    print("%d corresponds with Saturday" % (user_input))
+
